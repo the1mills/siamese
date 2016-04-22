40040699e3257e0f601846c0b331a12d83104902
the1mills <alexander.d.mills@gmail.com>
2016-04-21 20:07:21 -0700

    Fixed _reject and _resolve

--- index.js
+++ index.js
@@ -3,38 +3,38 @@
 // TODO: https://github.com/npm/nopt
 // TODO: https://github.com/nodejs/node-v0.x-archive/issues/8648
 
-function IdempotentJSON() {
+function Siamese() {
 }
 
-IdempotentJSON.prototype.parse = function (obj) {
+Siamese.prototype.parse = function (obj) {
 
     return Promise.resolve(obj).then(function (obj) {
         if (typeof obj !== 'string') {
             //warning: looks like you have called ijson.parse on an object that was already parsed
-            return _resolve(obj);
+            return Promise.resolve(obj);
         }
         else {
             var ret = JSON.parse(obj);
             if (typeof ret === 'object' && ('#stringified' in ret)) {
-                if(Object.keys(ret) > 1){
+                if(Object.keys(ret).length > 1){
                     console.error('Warning: object had more than 1 key, including #stringified key.');
                 }
                 ret = ret['#stringified'];
             }
-            return _resolve(ret);
+            return Promise.resolve(ret);
         }
     }).catch(function (err) {
-        return _reject(err);
+        return Promise.reject(err);
     });
 };
 
-IdempotentJSON.prototype.stringify = function (obj) {
+Siamese.prototype.stringify = function (obj) {
 
     return Promise.resolve(obj).then(function (obj) {
 
         if (typeof obj === 'string') {
             if (obj.indexOf('{"#stringified"') === 0) {
-                return _resolve(obj);
+                return Promise.resolve(obj);
             }
         }
 
@@ -42,33 +42,14 @@ IdempotentJSON.prototype.stringify = function (obj) {
             console.error('warning: object you wish to stringify with IJSON already has a top-level "#stringified" property.');
         }
 
-        return _resolve(JSON.stringify({'#stringified': obj}));
+        return Promise.resolve(JSON.stringify({'#stringified': obj}));
 
     }).catch(function (err) {
-        return _reject(err);
+        return Promise.reject(err);
     });
 
 };
 
-function _resolve(val) {
-
-    return new Promise(function (resolve) {
-        process.nextTick(function () {
-            resolve(val);
-        });
-    });
-
-}
-
-function _reject(err) {
-
-    return new Promise(function (resolve, reject) {
-        process.nextTick(function () {
-            reject(err);
-        });
-    });
-
-}
 
 
-module.exports = new IdempotentJSON();
+module.exports = new Siamese();
